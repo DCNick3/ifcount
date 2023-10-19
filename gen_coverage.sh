83354fad8b9c0a19dc2c44b7a57f785f4d2c53c2
BulbaWarrior <vladkalm49@gmail.com>
2023-10-20 00:58:41 +0300

    also remove profiling dumps from crates/**/*

--- gen_coverage.sh
+++ gen_coverage.sh
@@ -4,6 +4,6 @@ RUSTFLAGS="-C instrument-coverage" cargo test --profile cov --tests
 
 grcov . --binary-path ./target/cov/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 
-rm *.profraw
+rm *.profraw crates/**/*.profraw
 
 echo "coverage report: file://$PWD/target/coverage/html/index.html"
