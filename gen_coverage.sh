fa9c1f5fe93564c4d43f7a54062a5cf4422b6325
Nikita Strygin <nikita6@bk.ru>
2023-10-19 23:07:29 +0300

    Add a cov profile and add tests for `basic_enums`

--- gen_coverage.sh
+++ gen_coverage.sh
@@ -1,8 +1,8 @@
 rm -r target/coverage
 
-RUSTFLAGS="-C instrument-coverage" cargo test --tests
+RUSTFLAGS="-C instrument-coverage" cargo test --profile cov --tests
 
-grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
+grcov . --binary-path ./target/cov/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 
 rm *.profraw
 
--- src/collector/metrics/impl/basic_enums.rs
+++ src/collector/metrics/impl/basic_enums.rs
@@ -26,8 +26,9 @@ impl Visit<'_> for Enums {
     fn visit_item_enum(&mut self, i: &'_ syn::ItemEnum) {
         self.variant_count.observe(i.variants.len());
         self.attr_count.observe(i.attrs.len());
-        self.variant_attr_count
-            .observe(i.variants.iter().map(|x| x.attrs.len()).sum());
+        i.variants
+            .iter()
+            .for_each(|x| self.variant_attr_count.observe(x.attrs.len()));
         syn::visit::visit_item_enum(self, i);
     }
 }
@@ -40,4 +41,120 @@ pub fn make_collector() -> MetricCollectorBox {
         |v| Monoid::reduce(v.iter().cloned()),
     )
     .make_box()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::collector::metrics::r#impl::basic_enums::Enums;
+    use crate::collector::metrics::util::check;
+    use expect_test::expect;
+    use syn::parse_quote;
+
+    #[test]
+    fn no_enums() {
+        let code = parse_quote! {
+            struct Thing {
+                u: i32,
+            }
+        };
+        check::<Enums>(
+            code,
+            expect![[r#"
+            {
+              "variant_count": {
+                "sum": 0,
+                "avg": null,
+                "mode": null
+              },
+              "attr_count": {
+                "sum": 0,
+                "avg": null,
+                "mode": null
+              },
+              "variant_attr_count": {
+                "sum": 0,
+                "avg": null,
+                "mode": null
+              }
+            }"#]],
+        );
+    }
+
+    #[test]
+    fn small_enums() {
+        let code = parse_quote! {
+            #[derive(Debug, Clone, Copy)]
+            enum SmallEnum {
+                A,
+                B,
+                C,
+            }
+
+            #[derive(Debug, Clone)]
+            enum SmallEnum {
+                A,
+                B,
+            }
+        };
+        check::<Enums>(
+            code,
+            expect![[r#"
+            {
+              "variant_count": {
+                "sum": 5,
+                "avg": 2.5,
+                "mode": 3
+              },
+              "attr_count": {
+                "sum": 2,
+                "avg": 1.0,
+                "mode": 1
+              },
+              "variant_attr_count": {
+                "sum": 0,
+                "avg": 0.0,
+                "mode": 0
+              }
+            }"#]],
+        );
+    }
+
+    #[test]
+    fn big_enum() {
+        let code = parse_quote! {
+            #[derive(Debug, Clone, Copy)]
+            #[serde(tag = "type")]
+            enum BigEnum {
+                #[serde(rename = "a")]
+                A,
+                #[serde(rename = "b")]
+                B,
+                C,
+                D,
+                E,
+                F
+            }
+        };
+        check::<Enums>(
+            code,
+            expect![[r#"
+            {
+              "variant_count": {
+                "sum": 6,
+                "avg": 6.0,
+                "mode": 6
+              },
+              "attr_count": {
+                "sum": 2,
+                "avg": 2.0,
+                "mode": 2
+              },
+              "variant_attr_count": {
+                "sum": 2,
+                "avg": 0.3333333333333333,
+                "mode": 0
+              }
+            }"#]],
+        );
+    }
 }
